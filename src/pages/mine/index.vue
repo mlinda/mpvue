f4eb624d7d34ac60ae1d32261674a290fc954cc9
yanfeng <houyanfeng0911@rayootech.com>
2020-04-08 12:54:09 +0800

    init

---
+++ src/pages/mine/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>是的这是我的</p>
+    <div>这里你看到的是什么</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'mineIndex'
+  }
+</script>
+
+<style scoped>
+
+</style>
